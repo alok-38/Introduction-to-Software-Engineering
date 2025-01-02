c938739d3e4434474f24130067309a634bda4b1f
Alokananda Y <plantsfortrees@gmail.com>
2025-01-02 15:02:19 +0000

    Adding tailwind css

--- React/counter-app/src/Counter.jsx
+++ React/counter-app/src/Counter.jsx
@@ -9,10 +9,12 @@ const Counter = () => {
 
     return (
         <div>
-            <h2>Count: {count}</h2>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={reset}>Reset</button>
+            <h2 className="text-5xl">Count: {count}</h2>
+            <div className="flex justify-center gap-4 mt-4">
+                <button className="bg-indigo-500 text-white py-2 px-4 rounded-lg" onClick={increment}>Increment</button>
+                <button className="bg-amber-500 text-white py-2 px-4 rounded-lg" onClick={decrement}>Decrement</button>
+                <button className="bg-orange-600 text-white py-2 px-4 rounded-lg" onClick={reset}>Reset</button>
+            </div>
         </div>
     )
 }
