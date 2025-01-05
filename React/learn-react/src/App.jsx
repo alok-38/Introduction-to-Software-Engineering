a9ccb9bd14545bb73b52e0afcdfbc80cf8d33016
Alokananda Y <plantsfortrees@gmail.com>
2025-01-05 12:24:22 +0000

    Hedy Lamarr page

---
+++ React/learn-react/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import MyButton from './components/MyButton'
+import Profile from './components/Profile'
+import ShoppingList from './components/ShoppingList'
+
+function App() {
+
+  return (
+    <div>
+      <Profile />
+      <MyButton />
+      <ShoppingList />
+    </div>
+  )
+}
+
+export default App
