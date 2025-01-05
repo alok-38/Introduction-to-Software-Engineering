8cc310d4fd36141547b3a3905fdb2145266d9394
Alokananda Y <plantsfortrees@gmail.com>
2025-01-05 04:39:11 +0000

    Creating simple component

--- React/jsx-css/src/App.jsx
+++ React/jsx-css/src/App.jsx
@@ -4,35 +4,23 @@ const BookList = () => {
   return (
     <section>
       <Book />
+      <Book />
+      <Book />
+      <Book />
     </section>
   )
 }
 
 const Book = () => {
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className='book'>
+      <img
+        src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
+        alt='Interesting Facts For Curious Minds'
+      />
+      <h2>Interesting Facts For Curious Minds</h2>
+      <h4>Jordan Moore </h4>
     </article>
-  )
-}
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
-    alt='Interesting Facts For Curious Minds'
-  />
-);
-
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>;
-};
-const Author = () => <h4>Jordan Moore </h4>;
-
-const App = () => {
-  return (
-    <div>App</div>
   )
 }
 
---
+++ React/simple-component/app.js
@@ -0,0 +1,8 @@
+function HelloMessage({ name }) {
+    return <div>Hello {name}</div>;
+}
+
+// Get the root element from the DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HelloMessage name="Alok" />);
